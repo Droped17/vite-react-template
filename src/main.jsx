2a5ccd2d067074039ba984bc0a33bdbc62c5f3e9
Droped17 <droped17@gmail.com>
2023-09-08 11:12:07 +0700

    create Context

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { createContext,useContext } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { useState } from 'react'
+
+
+// Provider
+const AuthContext = createContext();
+
+function AuthContextProv(props){
+  return (
+    <>
+      <AuthContext.Provider>{props.children}</AuthContext.Provider>
+    </>
+  );
+}
+
+// Consumer
+
+
+
+function App(){
+
+  const [isAuth,setIsAuth] = useState(false);
+
+
+  function handleAuth(){
+    setIsAuth(!isAuth);
+  }
+
+  return (
+    <>
+      <div className='App'>
+        <h1>Welcome.. {!isAuth ? "Guest" : "User"}</h1>
+        <button onClick={handleAuth}>{!isAuth ? "Login" : "Logout"}</button>
+      </div>
+    </>
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <div>Vite React Template</div>
+    <App/>
 
 )
